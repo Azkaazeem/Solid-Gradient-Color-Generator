b84f07f1dc54be9a94adf2e3107b68ede93e98ac
Azkaazeem <azkaazeem804@gmail.com>
2025-11-13 20:14:09 +0500

    Gradient Half Complete

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ themeSwitch.addEventListener("click", () => {
 });
 
 let btn = document.querySelector("#generateBtn");
+let gradientBtn = document.querySelector("#generategradientBtn");
 
 // console.log (btn);
 
+
+// --------------------------   FOR SOLID COLOR    -------------------------------
 btn.addEventListener("click", function () {
     console.log("Btn is clicked....");
-    let h3 = document.querySelector("h3");
+    let h3 = document.querySelector("#heading1");
     let randomColor = getRandomColor();
     h3.innerText = randomColor;
 
@@ -19,9 +22,7 @@ btn.addEventListener("click", function () {
     div.style.backgroundColor = randomColor;
 
     console.log("Color Updated");
-
 });
-
 
 function getRandomColor() {
     let red = Math.floor(Math.random() * 255);
@@ -30,4 +31,36 @@ function getRandomColor() {
 
     let rgb = `rgb(${red}, ${green}, ${blue})`;
     return rgb;
+}
+
+
+// --------------------------   FOR GRADIENT COLOR    -------------------------------
+
+gradientBtn.addEventListener("click", function () {
+    console.log("Btn is clicked....");
+    let h3_2 = document.querySelector("heading");
+    let gradientRandomColor = getGradientRandomColor();
+    h3_2.innerText = gradientRandomColor;
+    h3_2.classList.add("fontChanges");
+
+    let div2 = document.getElementById("GradientColorBox");
+    div2.style.background = gradientRandomColor;
+
+    console.log("Color Updated");
+});
+
+
+
+
+function getGradientRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let red2 = Math.floor(Math.random() * 255);
+    let green2 = Math.floor(Math.random() * 255);
+    let blue2 = Math.floor(Math.random() * 255);
+
+    let grgb = `linear-gradient(to left, rgb(${red}, ${green}, ${blue}), rgb(${red2}, ${green2}, ${blue2}))`;
+    return grgb;
 }
